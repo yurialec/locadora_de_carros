daad75116cab1f9e12f899911a885e937be6e135
Yuri Alec <yurialec@hotmail.com>
2024-03-23 12:25:18 -0300

    288. Criando os models, controllers e migrations

---
+++ app/Models/Carro/Carro.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Carro;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carro extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Modelo/Modelo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Modelo;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Modelo extends Model
+{
+    use HasFactory;
+}
